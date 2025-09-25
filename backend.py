6269dca80dad534591bf3fcf462522ac0f980301
Mmadan128 <mohitmadan128@gmail.com>
2025-09-25 17:22:02 +0530

    deploy

--- backend.py
+++ backend.py
@@ -281,4 +281,4 @@ async def get_build_progress():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=int(os.getenv("PORT", 8000)))
